bffeaef232f6bd81bac965e82ff2cc65582ce1bb
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2022-03-11 16:34:22 +0545

    adds modal to data table in home page

--- components/dashboard/DataTable.jsx
+++ components/dashboard/DataTable.jsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import { useTable, useSortBy, useFilters, usePagination } from 'react-table';
 import { COLUMNS } from './column';
 
-const DataTable = () => {
+const DataTable = ({ showModal, setShowModal }) => {
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(
     () => [
@@ -59,7 +59,7 @@ const DataTable = () => {
   const { pageIndex } = state;
 
   return (
-    <>
+    <div className="relative">
       <table
         {...getTableProps()}
         className="min-w-full divide-y divide-gray-200"
@@ -70,7 +70,7 @@ const DataTable = () => {
               {headerGroup.headers.map((column) => (
                 <th
                   {...column.getHeaderProps(column.getSortByToggleProps())}
-                  class=" p-2 text-left text-sm font-bold text-gray-900 uppercase tracking-wider"
+                  className="p-2 text-sm font-bold tracking-wider text-left text-gray-900 uppercase "
                 >
                   <div className="flex justify-between">
                     {column.render('Header')}
@@ -83,7 +83,7 @@ const DataTable = () => {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            class="bi bi-sort-down"
+                            className="bi bi-sort-down"
                             viewBox="0 0 16 16"
                           >
                             <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z" />
@@ -94,7 +94,7 @@ const DataTable = () => {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            class="bi bi-sort-up"
+                            className="bi bi-sort-up"
                             viewBox="0 0 16 16"
                           >
                             <path d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z" />
@@ -106,7 +106,7 @@ const DataTable = () => {
                           width="16"
                           height="16"
                           fill="currentColor"
-                          class="bi bi-sort-up"
+                          className="bi bi-sort-up"
                           viewBox="0 0 16 16"
                         >
                           <path d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z" />
@@ -126,7 +126,11 @@ const DataTable = () => {
               <tr {...row.getRowProps()} className="pl-2 dashboard-data">
                 {row.cells.map((cell) => {
                   return (
-                    <td {...cell.getCellProps()} className="p-2 py-4">
+                    <td
+                      {...cell.getCellProps()}
+                      className="p-2 py-4"
+                      onClick={() => setShowModal(!showModal)}
+                    >
                       {cell.render('Cell')}
                     </td>
                   );
@@ -160,7 +164,7 @@ const DataTable = () => {
           Next
         </button>
       </div>
-    </>
+    </div>
   );
 };
 
--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import DataTable from '../components/dashboard/DataTable';
 import Navbar from '../components/layout/Navbar';
+import Modal from '../components/dashboard/modal';
 
 const home = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
       <Head>
@@ -13,7 +15,10 @@ const home = () => {
       </Head>
       <Navbar page={'Dashboard'} subPages={['Calendar', 'Admin']} />
       <div className="p-12 mx-6 -mt-6 bg-white rounded shadow h-3/5">
-        <DataTable />
+        <DataTable showModal={showModal} setShowModal={setShowModal} />
+        {showModal && (
+          <Modal setShowModal={setShowModal} showModal={showModal} />
+        )}
       </div>
     </>
   );

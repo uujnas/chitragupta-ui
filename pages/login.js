62dd1f3adbe96451ea3dc559db1f5c65c6ddebae
sagar535 <070bct535@ioe.edu.np>
2022-03-18 09:59:55 +0545

    captcha in login page

--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,23 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import React, { useState, useEffect, useRef } from "react";
 import {
   FormContainer,
   Input,
   Label,
   FormControl,
-  Btn
-} from '../components/formComponents';
+  Btn,
+} from "../components/formComponents";
 import { useRouter } from "next/router";
 import { verify_token } from "../components/routeGuard/RouteGuard";
 
 const Login = () => {
   const router = useRouter();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
   const token_verified = async () => {
-    console.log(process.env.NEXT_PUBLIC_REMOTE_URL);
     const token_verified = await verify_token();
     if (token_verified) {
       router.push(getRedirect());
@@ -31,53 +30,74 @@ const Login = () => {
       : "/home";
   };
 
+  const handleScoreResponse = async (score) => {
+    if (score > 0.7) {
+      try {
+        // make request to remote api login endpoint
+        const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_REMOTE_URL}/users/sign_in.json`,
+          {
+            user: { email, password },
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        localStorage.setItem("token", response.headers.authorization);
+        router.push(getRedirect());
+      } catch (error) {
+        localStorage.removeItem("token");
+        setError("Invalid user or password.");
+      }
+    } else {
+      setError("Are you a robot?");
+    }
+  };
+
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}`;
-    script.id = "recaptcha";
-    document.head.append(script);
-  });
+    const recaptcha_script = document.createElement("script");
+    recaptcha_script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}`;
+    recaptcha_script.id = "recaptcha";
+    document.head.append(recaptcha_script);
+
+    // clean up
+    return () => {
+      recaptcha_script.remove();
+    };
+  }, []);
 
   useEffect(token_verified, []);
 
   const handleSubmit = async (e) => {
-    console.log("handling submit");
     e.preventDefault();
 
+    // TODO: move the captcha logic to utils or libs
+    // the logic should be modular and needs implementation in other pages like password confirmations...
     // first we will check captcha score
-    grecaptcha.ready(function async () {
+    grecaptcha.ready(function async() {
       grecaptcha
         .execute(process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY, { action: "submit" })
         .then(function (token) {
-          console.log(token)
+          console.log(token);
           // We must make request to backend from here
           // if we try to force our way google simply won't let us
-          // console.log(token)
+          return axios.get(
+            `${process.env.NEXT_PUBLIC_REMOTE_URL}/api/v1/user_scores`,
+            { params: { token: token } }
+          );
         })
+        .then((response) => {
+          handleScoreResponse(response.data.score);
+        })
+        .catch((error) => {
+          setError(
+            error.response.data.message ||
+              error.response.data.error ||
+              error.message
+          );
+        });
     });
-
-    return;
-
-    setError("");
-
-    try {
-      // make request to remote api login endpoint
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_REMOTE_URL}/users/sign_in.json`,
-        {
-          user: { email, password },
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      localStorage.setItem("token", response.headers.authorization);
-      router.push(getRedirect());
-    } catch (error) {
-      localStorage.removeItem("token");
-      setError("Invalid user or password.");
-    }
   };
 
   return (
@@ -85,9 +105,9 @@ const Login = () => {
       <FormContainer>
         <div>
           {error.length >= 1 ? (
-              <div className="text-red-500 text">{error}</div>
+            <div className="text-red-500 text">{error}</div>
           ) : (
-              ""
+            ""
           )}
         </div>
         <FormControl>

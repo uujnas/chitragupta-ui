9ba11be0a693116d7055210b229f8874ea97540a
sagar535 <070bct535@ioe.edu.np>
2022-03-30 17:53:07 +0545

    removes export feature from leave requests

--- components/dashboard/LeaveRequestDataTable.jsx
+++ components/dashboard/LeaveRequestDataTable.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React from "react";
 import { CSVLink } from "react-csv";
 import { useGlobalContext } from "../../context";
 import DataTable from "./DataTable";
@@ -20,18 +20,7 @@ const LeaveRequestDataTable = ({
   const exportData = leaveRequests.map((d) => Object.values(d));
 
   return (
-    <DataTable data={leaveRequests} rowClick={rowClick} columns={columns}>
-      <div className="flex justify-end my-4">
-        <Btn className="bg-teal-500 hover:bg-teal-600">
-          <CSVLink
-            data={exportData}
-            filename={`${new Date().toISOString().slice(0, 18)}_report.csv`}
-          >
-            Export Data
-          </CSVLink>
-        </Btn>
-      </div>
-    </DataTable>
+    <DataTable data={leaveRequests} rowClick={rowClick} columns={columns} />
   );
 };
 

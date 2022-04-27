cf0536071a7089d2658a1d760e92bb1581f50bf8
niraj22 <nirajbudhathoki3@gmail.com>
2022-04-27 17:39:24 +0545

    Revert "Revert "implementing reducers""
    This reverts commit 5494674dfba454e9feae7ab013273800b56a4f44.

---
+++ actions/authActions.js
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import { returnErrors } from './errorActions'
+import {
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    SET_TOKEN
+} from './types'
+
+//*set initial token on page access
+export const setToken = (token) => (dispatch, getState) => {
+    dispatch({ type: SET_TOKEN, payload: token })
+}
+
+//*check token and load user
+export const loadUser = () => (dispatch, getState) => {
+    //user loading
+    dispatch({ type: USER_LOADING })
+
+    axios.get(`${process.env.NEXT_PUBLIC_REMOTE_URL}/api/v1/user.json`, tokenConfig(getState))
+    .then(res => dispatch({
+        type: USER_LOADED,
+        payload: res.data.data.attributes
+    }))
+    .catch(err => {
+        dispatch(returnErrors(err.response.data, err.response.status))
+        dispatch({ type: AUTH_ERROR })
+    });
+}
+
+//*log in user
+export const login = ({ email, password }) => dispatch => {
+    //headers
+    const config = {
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+    //request body
+    const body = JSON.stringify({user: {email, password}})
+    axios.post(`${process.env.NEXT_PUBLIC_REMOTE_URL}/users/sign_in.json`, body, config).then(res => {
+        dispatch({type: LOGIN_SUCCESS, payload: res})
+        dispatch({ type: SET_TOKEN, payload: res.headers.authorization })
+    })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL'))
+            dispatch({
+                type: LOGIN_FAIL
+            })
+        })
+
+}
+
+//*logout user
+export const logout = () => {
+    return {
+        type: LOGOUT_SUCCESS
+    }
+}
+
+
+
+//!setup config headers and token
+export const tokenConfig = getState => {
+    //get token from local storage
+    const token = getState().auth.token
+    //add it to the header
+    const config = {
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+    //if there is a token add to header
+    if (token) {
+        config.headers['Authorization'] = token;
+    }
+    return config
+}
--- components/routeGuard/RouteGuard.js
+++ components/routeGuard/RouteGuard.js
@@ -1,123 +1,37 @@
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import axios from "axios";
-import Jsona from "jsona";
-import { useGlobalContext } from "../../context";
+import { connect } from 'react-redux'
 
-const RouteGuard = ({ children }) => {
+const RouteGuard = (props) => {
   const router = useRouter();
-
-  const [authorized, setAuthorized] = useState(false);
-  const { user, setUser, loading } = useGlobalContext();
-
-  const isAdmin = () => user && user.role == "admin";
-
-  useEffect(() => {
-    const guard = async () => {
-      const token_verified = await verify_token();
-
-      if (token_verified) {
-        const response = await current_user();
-        const dataFormatter = new Jsona();
-        setUser(dataFormatter.deserialize(response.data));
-      }
-
-      // on initial load run auth check
-      await authCheck(router.asPath);
-
-      // on route change start - hide page content by setting authorized to false
-      const hideContent = () => setAuthorized(false);
-      router.events.on("routerChangeStart", hideContent);
-
-      // on route change complete - run auth check
-      router.events.on("routeChangeComplete", authCheck);
-
-      // unsubscribe from events in useEffect return function
-      return () => {
-        router.events.off("routeChangeStart", hideContent);
-        router.events.off("routeChangeComplete", authCheck);
-      };
-    };
-
-    guard();
-  }, [loading]);
-
-  // we need to pass url as there can be paths available to all
   // and paths only available to authenticated users
-  const authCheck = async (url) => {
+  const authCheck = (url) => {
     const publicPaths = ["/login"];
     const adminPaths = ["/admin", "/admin/users"];
 
     // why are we splitting with ?
     const path = url.split("?")[0];
 
-    const token_verified = await verify_token();
+    const token_verified = props.isAuthenticated;
 
     if (!token_verified && !publicPaths.includes(path)) {
       // first check if valid token is present
-      setAuthorized(false);
       router.push({
         pathname: "/login",
         // after login redirect to intended page
         query: { returnUrl: window.location.pathname }, // we are using window.location cuz router doesn't have access to query like id from here
       });
-    } else if (loading && !publicPaths.includes(path)) {
+    } else if (!publicPaths.includes(path)) {
       // while loading user don't authorize except for public paths
-      setAuthorized(false);
     } else if (!isAdmin() && adminPaths.includes(path)) {
       // guard for admin paths
-      setAuthorized(false);
       router.push("/");
-    } else {
-      setAuthorized(true);
     }
   };
-
-  return authorized && children;
+  return props.children
 };
 
-// validate auth token by hitting remote endpoint
-const verify_token = async () => {
-  try {
-    const response = await current_user();
-
-    return response.statusText == "OK";
-  } catch (error) {
-    return false;
-  }
-};
-
-// get current user
-const current_user = async () => {
-  try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_REMOTE_URL}/api/v1/user.json`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: localStorage.token,
-        },
-      }
-    );
-
-    return response;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-};
-
-RouteGuard.getInitialProps = async (ctx) => {
-  const token_verified = await verify_token();
-  // check that we are in SSR and not in Client side
-  if (
-    typeof window === "undefined" &&
-    ctx.ctx.res.writeHead &&
-    !token_verified
-  ) {
-    ctx.ctx.res.writeHead(302, { Location: "account/login" });
-    ctx.ctx.res.end();
-  }
-};
-
-export { RouteGuard, verify_token };
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error
+})
+export default connect(mapStateToProps)(RouteGuard)

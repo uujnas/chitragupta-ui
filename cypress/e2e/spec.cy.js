023bf0c45a76341bcd7dc1c4eccad41a40f36a14
sagar535 <070bct535@ioe.edu.np>
2022-06-10 17:50:15 +0545

    begins cypress e2e testing

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,31 @@
+describe('Navigation', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.contains('Login')
+
+    cy.contains('E-mail')
+    cy.contains('Password')
+    cy.contains('Forgot Password')
+
+    // write email
+    cy.get('[type=email]').type('dit.subas1@gmail.com')
+    cy.get('[type=password]').type('foobar')
+
+    cy.get('button').click()
+
+    cy.contains('Dashboard')
+    cy.contains('Calendar')
+    cy.contains('Admin')
+
+    cy.get('[href="/admin"]').click()
+    cy.contains('User')
+    cy.contains('Salary')
+    cy.contains('Salary Records')
+
+
+    cy.get('button').first().click()
+  })
+})

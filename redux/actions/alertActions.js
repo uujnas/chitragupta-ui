977a5d0e7a3984be4debd5e12fbedb12abe2cfac
sagar535 <070bct535@ioe.edu.np>
2022-06-15 15:00:46 +0545

    show Internal server error on status 500

--- redux/actions/alertActions.js
+++ redux/actions/alertActions.js
@@ -4,10 +4,17 @@ import {
   GET_SUCCESS_ALERT
 } from "./types"
 // RETURN ERRORS
-export const returnErrors = (message, status, id = null) => ({
-    type: GET_ERRORS_ALERT,
-    payload: { message, status, id }
-  })
+export const returnErrors = (message, status, id = null) => {
+    console.log(message)
+    message = status === 500 ? 'Internal Server Error' : message
+    // debugger
+    return {
+        type: GET_ERRORS_ALERT,
+        payload: {
+            message, status, id
+        }
+    }
+}
 
 // RETURN ALERTS
 export const returnAlerts = (message, status, id = null) => ({

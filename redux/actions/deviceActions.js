00b2c7738e36ebbdaefa40a905f7a76ac17f5fa6
Sanjeev <sanjeevpoudel111@gmail.com>
2022-06-20 12:14:56 +0545

    Add deviceActions

---
+++ redux/actions/deviceActions.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import axios from 'axios'
+import { returnAlerts, returnErrors } from './alertActions'
+
+export const createDevice = (device) => async (dispatch, getState) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_REMOTE_URL}/api/v1/devices.json`,
+      {
+        device,
+      },
+      {
+        headers: {
+          Authorization: getState().auth.token,
+        },
+      },
+    )
+    dispatch(
+      returnAlerts(
+        'Successfully created new device',
+        response.status,
+        'RECORD_CREATION_SUCCESS',
+      ),
+    )
+  } catch (error) {
+    dispatch(
+      returnErrors(
+        error.response && error.response.data,
+        error.resposne && error.response.status,
+      ),
+    )
+  }
+}

2270939f0291d315d34583bc922a16ccc904b00a
Sanjeev <sanjeevpoudel111@gmail.com>
2022-06-21 13:42:23 +0545

    Add deviceType redux actions

---
+++ redux/actions/deviceTypeActions.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import axios from 'axios'
+import { returnAlerts, returnErrors } from './alertActions'
+
+export const createDeviceType = (deviceType) => async (dispatch, getState) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_REMOTE_URL}/api/v1/device_types.json`,
+      {
+        deviceType,
+      },
+      {
+        headers: {
+          Authorization: getState().auth.token,
+        },
+      },
+    )
+    dispatch(
+      returnAlerts(
+        'Successfully created new device type',
+        response.status,
+        'RECORD_CREATION_SUCCESS',
+      ),
+    )
+  } catch (error) {
+    dispatch(
+      returnErrors(
+        error.response && error.response.data,
+        error.resposne && error.response.status,
+      ),
+    )
+  }
+}

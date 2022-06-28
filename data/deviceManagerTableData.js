0d3c45c3037179890035a1384e74f6851c94d770
Sanjeev <sanjeevpoudel111@gmail.com>
2022-06-28 17:43:14 +0545

    Fix: columns data for device managers table

--- data/deviceManagerTableData.js
+++ data/deviceManagerTableData.js
@@ -1,19 +1,31 @@
 export const columns = [
   {
     Header: 'Device',
-    accessor: 'device',
+    accessor: 'device_id',
+    id: 'device_id',
+    Cell: ({ row }) => {
+      const { device } = row.original
+      return `${device.identifier}`
+    },
   },
 
   {
-    Header: 'User',
-    accessor: 'user',
+    Header: 'Manager',
+    accessor: 'Username',
+    id: 'username',
+    Cell: ({ row }) => {
+      const { user } = row.original
+      return `${user.first_name} ${user.last_name}`
+    },
   },
+
   {
     Header: 'Assigned at',
-    accessor: 'assigned at',
+    accessor: 'assigned_at',
   },
+
   {
     Header: 'Unassigned at',
-    accessor: 'unassigned at',
+    accessor: 'unassigned_at',
   },
 ]

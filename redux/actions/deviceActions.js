ccde737f27cc911acfd5a88899bab8a88e683911
Sanjeev <sanjeevpoudel111@gmail.com>
2022-06-30 14:23:33 +0545

    Add formData for image upload

--- redux/actions/deviceActions.js
+++ redux/actions/deviceActions.js
@@ -3,15 +3,16 @@ import axios from 'axios'
 import { returnAlerts, returnErrors } from './alertActions'
 
 export const createDevice = (device) => async (dispatch, getState) => {
+  const formData = new FormData()
+
+  for (const field in device) {
+    formData.append(`device[${field}]`, device[field])
+  }
+  formData.append('device[image]', document.querySelector('#image').files[0])
   try {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_REMOTE_URL}/api/v1/devices.json`,
-      {
-        device: {
-          ...device,
-          device_type_attributes: device_type,
-        },
-      },
+      formData,
       {
         headers: {
           Authorization: getState().auth.token,

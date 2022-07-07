4af5f5b379a73238722ac7d5e34816aede22b30b
uujnas <sanjubasnet036@gmail.com>
2022-07-07 12:39:10 +0545

    chore: group lunch of particular day

--- data/groupLunchDayTableData.js
+++ data/groupLunchDayTableData.js
@@ -1,10 +1,13 @@
 export const columns = [
   {
-    Header: 'Id',
-    accessor: 'id',
-  },
-  {
     Header: 'date',
     accessor: 'date',
+  },
+  {
+    Header: 'Group Lunch',
+    accessor: (originalRow) =>
+      originalRow.group_lunches
+        .map((group_lunches) => `GroupLunch-${group_lunches.id}`)
+        .join(', '),
   },
 ]

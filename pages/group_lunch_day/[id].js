f10730dee69c98ffa221897724edbf28397581df
uujnas <sanjubasnet036@gmail.com>
2022-07-08 17:10:36 +0545

    ui change

--- pages/group_lunch_day/[id].js
+++ pages/group_lunch_day/[id].js
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import Navbar from '../../components/layout/Navbar'
+import { TableContainer } from '../../components/modalComponents'
 
 const GroupLunchDayDetail = () => {
   const router = useRouter()
@@ -9,35 +10,34 @@ const GroupLunchDayDetail = () => {
 
   const [lunchDay, setLunchDay] = useState({})
 
-    useEffect(() => {
-      const fetchGroupLunchDay = () => {
-        axios
-          .get(
-            `${process.env.NEXT_PUBLIC_REMOTE_URL}/api/v1/group_lunch_days/${id}`,
-            {
-              headers: {
-                'Content-type': 'application/json',
-                Authorization: localStorage.token,
-              },
+  useEffect(() => {
+    const fetchGroupLunchDay = () => {
+      axios
+        .get(
+          `${process.env.NEXT_PUBLIC_REMOTE_URL}/api/v1/group_lunch_days/${id}`,
+          {
+            headers: {
+              'Content-type': 'application/json',
+              Authorization: localStorage.token,
             },
-          )
-          .then((response) => setLunchDay(response.data.data))
-          .catch((err) => console.log(err))
-      }
-      fetchGroupLunchDay();
-    },[])
+          },
+        )
+        .then((response) => setLunchDay(response.data.data))
+        .catch((err) => console.log(err))
+    }
+    fetchGroupLunchDay()
+  }, [])
 
   return (
     <>
       <Navbar />
-      {lunchDay?.attributes?.date}
-      {
-        lunchDay?.relationships?.group_lunches?.data?.map((group_lunch) =>
-          <ul key = {group_lunch.id}>
-            <li> Group Lunch { group_lunch.id}</li>
+      <TableContainer>
+        {lunchDay?.relationships?.group_lunches?.data?.map((group_lunch) => (
+          <ul key={group_lunch.id}>
+            <li> Group Lunch {group_lunch.id}</li>
           </ul>
-        )
-      }
+        ))}
+      </TableContainer>
     </>
   )
 }

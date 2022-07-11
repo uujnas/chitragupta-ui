fbd45fb9644f8eaeb4bccef73c061d0915a56b73
uujnas <sanjubasnet036@gmail.com>
2022-07-11 13:40:45 +0545

    ui for group lunch day show action

--- pages/group_lunch_day/[id].js
+++ pages/group_lunch_day/[id].js
@@ -24,24 +24,83 @@ const GroupLunchDayDetail = () => {
         .then((response) =>
           setGroupLunchDay({
             date: response.data.data.attributes.date,
-            group_lunch: response.data.data.relationships.group_lunches.data,
-          }),
+            group_lunch: response.data.included,
+          })
         )
+
         .catch((err) => console.log(err))
     }
     fetchGroupLunchDay()
   }, [])
+
 
   return (
     <>
       <Navbar />
       <TableContainer>
         <strong>Date: {lunchDay.date}</strong>
-        {lunchDay?.group_lunch?.map((group_lunch) => (
-          <ul key={group_lunch.id}>
-            <li onClick={ () => router.push(`../group_lunch/${group_lunch.id}`)}> Group Lunch {group_lunch.id}</li>
-          </ul>
-        ))}
+        <div className="flex flex-col mt-8">
+          <div className="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+            <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+              <table className="min-w-full">
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 text-sm tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Group Lunch
+                    </th>
+                    <th className="px-6 py-3 text-sm tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Status
+                    </th>
+                    <th className="px-6 py-3 text-sm tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                      Group Member
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody className="bg-white">
+                  {lunchDay?.group_lunch?.map((lunch_data) => (
+                    <tr key={ lunch_data.id}
+                      className="hover:cursor-pointer"
+                      onClick={() =>
+                        router.push(`../group_lunch/${lunch_data.id}`)
+                      }
+                    >
+                      <td className="px-6 py-4 whitespace-no-wrap">
+                        <div className="flex">
+                          <img
+                            className="w-12 h-12 rounded-full"
+                            src={`${lunch_data.attributes.lunch_img_urls}`}
+                            onError={(e) => {
+                              e.target.style.display = 'none'
+                            }}
+                            alt="group lunch"
+                          />
+                          <div className="ml-4">{lunch_data.id}</div>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap">
+                        {`${lunch_data.attributes.status}`}
+                      </td>
+                      <td className="px-6 py-4  whitespace-no-wrap">
+                        {lunch_data?.attributes?.group_member
+                          ?.map((member) => (
+                            <span key={member.id}>{member.user_id}</span>
+                          ))
+                          .reduce(
+                            (group_member, elem) =>
+                              group_member === null
+                                ? [elem]
+                                : [...group_member, ', ', elem],
+                            null,
+                          )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </TableContainer>
     </>
   )
